fbeff71c1a95272fe784ede21f711bd4db35c941
Pratham S <snehi.pratham@gmail.com>
2024-10-10 09:28:18 -0500

    add the watermark functionality

--- tkinter_ui.py
+++ tkinter_ui.py
@@ -1,6 +1,6 @@
 import customtkinter as tk
 from sqlalchemy import column
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from tkinter import filedialog
 
 
@@ -28,6 +28,7 @@ class App(tk.CTk):
         # 2. Frame with "Add Watermark" on the bottom
         self.watermark_frame = AddWatermarkFrame(self)
         self.watermark_frame.grid(row=1, column=0, sticky="ew", padx=10, pady=10)
+        self.watermark_frame.button_add_watermark.configure(command=lambda: self.watermark_frame.apply_watermark(img_path=self.upload_img_frame.preview_img, watermark_text=self.watermark_frame.watermark_entry.get()))
 
 
 class AddWatermarkFrame(tk.CTkFrame):
@@ -43,6 +44,25 @@ class AddWatermarkFrame(tk.CTkFrame):
         self.button_add_watermark.grid(row=0, column=1, padx=20, pady=20, sticky="ew")
         self.grid_columnconfigure(0, weight=1)
 
+    def apply_watermark(self, img_path, watermark_text):
+        base = img_path.convert("RGBA")
+        # make a blank image for the text, initialized to transparent text color
+        txt = Image.new("RGBA", base.size, (255, 255, 255, 0))
+
+        # get a font
+        fnt = ImageFont.truetype("Roboto-Black.ttf", 60)
+        # get a drawing context
+        d = ImageDraw.Draw(txt)
+
+        # draw text, half opacity
+        d.text((0, 0), text=watermark_text, font=fnt, fill=(255, 255, 255, 64))
+        # # draw text, full opacity
+        # d.text((10, 60), text=watermark_text, font=fnt, fill=(255, 255, 255, 255))
+
+        out = Image.alpha_composite(base, txt)
+
+        out.show()
+
 
 def get_photo_file_path():
     return filedialog.askopenfilename()
@@ -52,6 +72,7 @@ class UploadImageFrame(tk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
+        self.img_preview_label = None
         self.preview_img = None
         self.preview_img_element = None
         self.photo_filepath = None
@@ -73,7 +94,6 @@ class UploadImageFrame(tk.CTkFrame):
         # creating a new label in order to display the image preview
         self.img_preview_label = tk.CTkLabel(self, text="", image=self.preview_img_element)
         self.img_preview_label.grid(row=1, column=0)
-
 
 
 if __name__ == "__main__":
